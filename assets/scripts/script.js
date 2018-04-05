98fac237fca7e0edd42c39ea6ccbe441e3ae8de1
KristianEllgaardJonsson <kristian.jonsson2@gmail.com>
2018-04-05 14:27:10 +0200

    maps

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 57.0488, lng: 9.9217},
+          zoom: 14,
+            disableDefaultUI: true
+        });
+      }
